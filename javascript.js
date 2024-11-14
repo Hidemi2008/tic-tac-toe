a24bf0316e12719997cacdae91f3d05c579a9ae9
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-11-14 09:44:09 -0400

    Complete the page, but i use the videou from youtube for this

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message
+    }
+    return {
+        renderMessage,
+    }
+})()
+
 const gameBoard = (() =>{
 let gameboard = ["","","","","","","","",""]
 
@@ -6,11 +15,25 @@ let gameboard = ["","","","","","","","",""]
         gameboard.forEach((square, index) => {
             boardHTML += `<div class="square" id=square-${index}">${square}</div>`
         })
+        document.querySelector("#gameboard").innerHTML = boardHTML
+        const squares = document.querySelectorAll(".square")
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick)
+        })
     }
-    document.querySelector("#gameboard").innerHTML = boardHTML
+
+const update = (index,value) => {
+    gameboard[index] = value
+    render()
+}
+ 
+const getGameboard = () => gameboard
+
 
     return{
         render,
+        update,
+        getGameboard
     }
 
 })();
@@ -24,19 +47,95 @@ const createPlayer= (name,mark) => {
 
 const Game = (() => {
     let players = []
-    let currentPlayersIndex = 0
-    let gameOver = false
+    let currentPlayersIndex
+    let gameOver
 
-    const star = () => {
+    const start = () => {
         players = [
-
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
         ]
+        currentPlayersIndex = 0
+        gameOver = false
+        gameBoard.render()
+        const squares = document.querySelectorAll(".square")
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick)
+        })
+        }
+
+        const handleClick = (event) => {
+            if(gameOver){
+                return 
+            }
+            let index = parseInt(event.target.id.split("-")[1])
+
+            if (gameBoard.getGameboard()[index] !== "")
+                return;
+            
+                
+            
+            gameBoard.update(index, players
+            [currentPlayersIndex].mark)
+
+
+            if(checkForWin(gameBoard.getGameboard(), players[currentPlayersIndex].mark)){
+                gameOver = true
+                displayController.renderMessage(`${players[currentPlayersIndex].name} wins`)
+            }else if(checkForTie(gameBoard.getGameboard())){
+                gameOver = true
+            displayController.renderMessage(`It's tie`)
+            }
+
+            currentPlayersIndex = currentPlayersIndex === 0 ? 1 : 0
+        }
+
+        const restart = () => {
+            for(let i = 0; i < 9; i++){
+                gameBoard.update(i, "")
+            }
+            gameBoard.render()
+            gameOver = false
+            document.querySelector("#message").innerHTML = ""
+        }
+
+        return{
+            start,
+            restart,
+            handleClick
+        }
+})();
+
+function checkForWin(board){
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for(let i = 0; i < winningCombinations.length; i++){
+        const [a,b,c] = winningCombinations[i]
+        if (board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true
         }
     }
+    return false
+}
 
+function checkForTie(board){
+    return board.every(cell => cell !== "")
+}
+
+const restartBnt = document.querySelector("#bnt-restart")
+restartBnt.addEventListener("click", () =>{
+    Game.restart()
 })
 
 const startBnt = document.querySelector("#bnt-start")
 startBnt.addEventListener("click", () => {
-
+    Game.start()
 })
